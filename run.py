f3a7514aefb2c6c684cca33f5c1f2a2efdb4493c
Aniruddha Mandal <ani96dh@gmail.com>
2021-06-28 16:27:06 +0530

    Data Output Directory Changed and Modulated.

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
 from Downloder import data_downloder
 from Downloder import config
+import os
+
+try:
+    os.mkdir("Data")
+except:
+    print("Data directory already present in the current folder.")
+
+
 data_downloder.download_data(config.LIST_OF_STOCKS)
